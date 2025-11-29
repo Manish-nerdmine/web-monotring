c623b433bf99a0213b13e54c092ce616a6e047ae
Manish <136878023+Manish-nerdmine@users.noreply.github.com>
2025-11-29 21:48:27 +0530

    add

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,10 +1,12 @@
+```javascript
 module.exports = {
     apps: [
         {
             name: "dark-web-monitoring",
             script: "pnpm",
-            args: "run dev -- --host 13.50.233.20 --port 5173",
+            args: "run dev --host 13.50.233.20 --port 5173",
             interpreter: "none"
         }
     ]
 }
+```
